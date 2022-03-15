7f5ed2252904af8dd313bcd579218ebffd5c5a3b
Utkarsh <utripathi2002@gmail.com>
2022-03-15 19:06:16 +0530

    mail added

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const app = express();
+const nodemailer = require("nodemailer");
 
 const dbUrl = process.env.DB_URL;
 const connectDB = async () => {
@@ -19,6 +20,17 @@ const connectDB = async () => {
 connectDB()
   .then(() => console.log("Connected to Database"))
   .catch((e) => console.log(e));
+
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 app.use(morgan("dev"));
 app.use(cors());
@@ -57,11 +69,29 @@ app.post("/api/contact", async (req, res) => {
     const newContact = new Contact(data);
     await newContact.save();
     console.log("done");
+    await transporter
+      .sendMail({
+        from: `"Utkarsh" <utripathi_be21@thapar.edu>`,
+        to: "utripathi2002@gmail.com",
+        subject: "New Contact Request from Portfolio",
+        html: `From ${data.name} <br> Email: ${data.email} <br> Type: ${data.type} <br> Description: ${data.description}`,
+      })
+      .then(() => console.log("mail sent"));
     res.status(200).json({ status: "ok" });
   } catch (e) {
     console.log(e);
     res.status(200).json({ status: "not-ok" });
   }
 });
+
+app.get("/api/getContacts", async (req, res) => {
+  try {
+    const data = await Contact.find({});
+    res.status(200).json({ data }).send(data);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening ${port}`));
